528536ff787adabb2f1f2f1f3bea66e48217231d
Kei <motosjairo12@gmail.com>
2024-06-20 13:37:49 +0200

    firebase updated chat&user

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,11 +1,38 @@
-import { useState } from "react"
-import "./chatList.css"
-import AddUser from "./addUser/AddUser"
+import { useEffect, useState } from "react";
+import "./chatList.css";
+import AddUser from "./addUser/AddUser";
+import { useUserStore } from "../../../lib/userStore";
+import { doc, getDoc, onSnapshot } from "firebase/firestore";
+import { db } from "../../../lib/firebase";
 
 const ChatList = () => {
+  const [addMode, setAddMode] = useState(false);
+  const [chats, setChats] = useState([]);
 
-  const [addMode, setAddMode] = useState(false)
+  const { currentUser } = useUserStore();
 
+  useEffect(() => {
+    const unSub = onSnapshot(doc(db, "userchats", currentUser.id), async(res) => {
+      const items = res.data().chats;
+
+      const promises = items.map( async(item) => {
+        const userDocRef = doc(db, 'users', item.receiverId);
+        const userDocSnap = await getDoc(userDocRef);
+
+        const user = userDocSnap.data();
+
+        return {...item, user}
+      });
+
+      const chatData = await Promise.all(promises);
+
+      setChats(chatData.sort((a,b) => b.updatedAt - a.updatedAt ));
+    });
+
+    return () => {
+      unSub();
+    };
+  }, [currentUser.id]);
 
   return (
     <div className="chatList">
@@ -14,43 +41,24 @@ const ChatList = () => {
           <img src="/search.png" alt="" />
           <input type="text" placeholder="Search..." />
         </div>
-        <img 
-          onClick={()=> setAddMode( (prev) => !prev )}
-          src={addMode ? "./minus.png" : "./plus.png"} 
+        <img
+          onClick={() => setAddMode((prev) => !prev)}
+          src={addMode ? "./minus.png" : "./plus.png"}
           className="add"
         />
       </div>
-      <div className="item">
-        <img src="./avatar.png" alt="" />
-        <div className="texts">
-          <span>Nala Dixon</span>
-          <p>Hello</p>
+      {chats.map((chat) => (
+        <div className="item" key={chat.chatId}>
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>Nala Dixon</span>
+            <p>{chat.lastMessage}</p>
+          </div>
         </div>
-      </div>
-      <div className="item">
-        <img src="./avatar.png" alt="" />
-        <div className="texts">
-          <span>Nala Dixon</span>
-          <p>Hello</p>
-        </div>
-      </div>
-      <div className="item">
-        <img src="./avatar.png" alt="" />
-        <div className="texts">
-          <span>Nala Dixon</span>
-          <p>Hello</p>
-        </div>
-      </div>
-      <div className="item">
-        <img src="./avatar.png" alt="" />
-        <div className="texts">
-          <span>Nala Dixon</span>
-          <p>Hello</p>
-        </div>
-      </div>
-      { addMode && <AddUser />}
+      ))}
+      {addMode && <AddUser />}
     </div>
-  )
-}
+  );
+};
 
-export default ChatList
+export default ChatList;
